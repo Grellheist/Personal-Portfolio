2f223ef7a4e68fbba4c0a8911d08a772a6515062
Grellheist <grellheist@tutanota.com>
2023-03-10 10:21:42 -0300

    Updated

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
@@ -23,6 +23,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/download_cv")
+def download():
+    return send_file("cv/curriculo.pdf", as_attachment=True)
 
 
 if __name__ == "__main__":

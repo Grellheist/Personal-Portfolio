adc31bcbe94b5f85e37056ee81e5963195c23eb4
grellheist <grellheist@tutanota.com>
2023-05-18 23:56:30 -0300

    Added about content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div>
+            <h1>About me</h1>
+            <p>Olá! Meu nome é Victor Hugo (também sou conhecido como só Hugo). Atualmente trabalho como Administrador/Operador de Redes. Faço faculdade de Engenharia de Computação na UFG (com o curso atrasado por diversos motivos, mas provavelmente terminarei em 2024). Sou fluente em inglês.
+            </p>
+            <p>Sou um cara apaixonado por tecnologia, sempre tentando descobrir como as coisas funcionam e sempre tentando melhorar e aprender mais coisas. Sou fã de resolver problemas (meu trabalho atual que o diga). No meu tempo livre sempre tento aprender algo novo. Quando não aprendo algo novo, provavelmente estou jogando.
+            </p>
+        </div>
+    )
 }

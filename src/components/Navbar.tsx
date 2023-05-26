bde2422937ed6fa68a3e2353068f7a5da5382a2c
grellheist <grellheist@tutanota.com>
2023-05-25 23:31:01 -0300

    Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link } from "react-scroll/modules"
 import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
@@ -33,12 +33,12 @@ export const Navbar = () => {
             <div className='justify-between md:items-center md:flex'>
                 <div>
                     <div className='md:py-5 md:block'>
-                        <h2 className='text-2xl font-bold'>Victor Hugo Buntrok</h2>
+                        <h2 className='text-2xl font-bold dark:text-neutral-100'>Victor Hugo Buntrok</h2>
                     </div>
                 </div>
                 <div className='md:flex md:space-x-6'>
                     {NAV_ITEMS.map((item, index) => {
-                        return <a key={index} href="">{item.label}</a>
+                        return <a className="block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100" key={index} href="">{item.label}</a>
                     })}
                     {currentTheme === "dark" ? (
                         <button className='bg-slate-100 p-2 rounded-xl' onClick={() => setTheme("light")}>
